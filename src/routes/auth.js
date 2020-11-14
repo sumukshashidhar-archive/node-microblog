97adf3da6d2bf78cb6278a70c9e13170f59ab1f6
sumukshashidhar <sumukuuu@gmail.com>
2020-11-14 23:07:39 +0530

    some routing simplification

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,9 @@
+const ams = require("./../controllers/auth_microservice")
 module.exports = function (app) {
     app.post('/login', function (req, res) {
-        
+        let response = ams.login(req.body.username, req.body.password)
+        if(response){
+
+        }
     })
 }
