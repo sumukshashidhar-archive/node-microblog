00a1e9d6d9e5b17255847e7e939d48e48361c379
sumukshashidhar <sumukuuu@gmail.com>
2020-11-14 22:30:51 +0530

    fixed the nanoid stuff

--- src/models/post.js
+++ src/models/post.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-import { nanoid } from 'nanoid'
+const nanoid = require('nanoid')
 
 const postSchema = new mongoose.Schema({
-    post_id: {type: String, default: nanoid()},
+    post_id: {type: String, default: nanoid.nanoid()},
     user_id: String,
     post_title: String,
     post_time: {type: Date, default: Date.now()},
