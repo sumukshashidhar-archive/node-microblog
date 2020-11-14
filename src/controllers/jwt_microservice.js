56200e4487e866cf0c2f0f6171a811094e1423b8
sumukshashidhar <sumukuuu@gmail.com>
2020-11-14 23:14:34 +0530

    added imports

--- src/controllers/jwt_microservice.js
+++ src/controllers/jwt_microservice.js
@@ -1,4 +1,17 @@
 const jwt = require('jsonwebtoken')
+const fs = require("fs");
+const path = require("path");
+
+const publicKeyPath = path.join(__dirname, "./../keys/public.pem");
+const privateKeyPath = path.join(__dirname, "./../keys/private.pem");
+
+// key imports
+const publicKEY = fs.readFileSync(publicKeyPath, "utf-8");
+const privateKEY = fs.readFileSync(privateKeyPath, "utf-8");
+
+// options
+const jENV = require("./../config/tokenOptions");
+
 module.exports = {
     verification: async function (token) {
         return new Promise(async (resolve, reject) => {
