03d20fb9ef6ef0c8a9843e010e52a16e6735b4aa
sumukshashidhar <sumukuuu@gmail.com>
2020-11-14 14:10:42 +0530

    adding some controllers nad models for posts

--- src/routes.js
+++ src/routes.js
@@ -3,4 +3,5 @@ module.exports = function(app) {
     require("./routes/auth")(app); // authentication routes
     require("./routes/dashboard")(app); // dashboard routes
     require("./routes/data-vault")(app); // vault
+    require("./routes/api")(app);
 }
---
+++ src/routes/api.js
@@ -0,0 +1,6 @@
+const logger = require("./../config/logger")
+module.exports = function (app) {
+    app.get('/api/getPosts', async function(req, res) {
+
+    })
+}
