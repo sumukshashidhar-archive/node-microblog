2b7f5e7b9ae77746450c4c8703c2b202dd178a47
sumukshashidhar <sumukuuu@gmail.com>
2020-11-14 14:18:49 +0530

    added the get all posts function

--- src/controllers/posts_microservice.js
+++ src/controllers/posts_microservice.js
@@ -14,5 +14,20 @@ module.exports = {
                 }
             })
         })
+    },
+
+    getAllPosts: async function(user_id) {
+        return new Promise(async function(resolve, reject) {
+            post.find({user_id: user_id}, async function(err, obj) {
+                if(err) {
+                    logger.error("Failed to connect to mongodb")
+                    reject(false)
+                }
+                else {
+                    logger.debug(`Returning ${obj} for ${user_id}`)
+                    resolve(obj)
+                }
+            })
+        })
     }
 }
