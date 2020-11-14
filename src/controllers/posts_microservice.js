048861375c8c8ed5a51c03db5e6104e4838f5037
sumukshashidhar <sumukuuu@gmail.com>
2020-11-14 14:13:06 +0530

    added post controller

---
+++ src/controllers/posts_microservice.js
@@ -0,0 +1,9 @@
+const logger = require("./../config/logger")
+const post = require("./../models/post")
+module.exports = {
+    getRecentPosts: async function(user_id) {
+        return new Promise(async function(resolve, reject) {
+
+        })
+    }
+}
