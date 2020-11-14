6dd57aa6cb402803deb2dc78b33ad2dd29f12708
sumukshashidhar <sumukuuu@gmail.com>
2020-11-14 14:18:20 +0530

    adding some more functions

--- src/controllers/posts_microservice.js
+++ src/controllers/posts_microservice.js
@@ -3,7 +3,16 @@ const post = require("./../models/post")
 module.exports = {
     getRecentPosts: async function(user_id) {
         return new Promise(async function(resolve, reject) {
-
+            post.find({user_id: user_id}, async function(err, obj) {
+                if(err) {
+                    logger.error("Failed to connect to mongodb")
+                    reject(false)
+                }
+                else {
+                    logger.debug(`Returning ${obj} for ${user_id}`)
+                    resolve(obj)
+                }
+            })
         })
     }
 }
