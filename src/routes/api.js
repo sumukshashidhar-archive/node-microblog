e5996c59a2bc224ddd4a0760d88f9d597728ed4f
sumukshashidhar <sumukuuu@gmail.com>
2020-11-14 14:19:59 +0530

    halting dev

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 const logger = require("./../config/logger")
+const pms = require("./../controllers/posts_microservice")
 module.exports = function (app) {
-    app.get('/api/getPosts', async function(req, res) {
-
+    app.get('/api/getAllPosts', async function(req, res) {
+        pms.getAllPosts()
     })
 }
